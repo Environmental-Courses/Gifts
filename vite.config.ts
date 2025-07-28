b6b33aa485d6c8a1e50da8a52b177c62d5bd1f6c
Polemarchos56 <131057842+Polemarchos56@users.noreply.github.com>
2025-07-28 17:39:07 +0300

    Update content

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  base: '/Gifts/', // Must match your EXACT repository name
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false // Disable for production
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
   },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 3000 // Optional: Set your preferred port
-  }
-});
+}));
